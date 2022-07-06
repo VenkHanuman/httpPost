68aa16f5defa40239fd1efa325f20662207b865a
VenkHanuman <venkateshkv@yahoo.com>
2022-07-06 17:26:20 +0530

    Removed scret keys

--- src/app/InmarsatLogin.ts
+++ src/app/InmarsatLogin.ts
@@ -1,5 +1,5 @@
 export class InmarsatLogin {
-    clientId : string  = "0oa4eb6w2ffgEkFcA0i7";
-    clientSecret : string = "E8G_V5-XwDs784G9eqdka4a0yLUJwYC8b08hw6OW";
+    clientId : string  = "ClientId";
+    clientSecret : string = "ClientSecret";
  
 }
